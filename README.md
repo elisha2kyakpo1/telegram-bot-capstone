360d8eece1dc693e594fd5d7bf64f65edc59a155
elisha2kyakpo1 <kyakopoe@gmail.com>
2020-12-15 16:50:01 +0200

    added a readme file

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # twitter-bot-capstone
+
+> ### This bot is designed to send daily motivational messages and tell jokes every time the user types their name
+
+## Built With
+1.	__Ruby__ 
+2.	__Rubocop__
+3.	__Rspec__
+4.	__telegram-bot-ruby library__
+
+## Getting Started
+To get a local copy up and running follow these simple example steps.
+
+Open the terminal and type
+git clone https://github.com/elisha2kyakpo1/telegram-bot-capstone
+ 
+## To use the bot:
+> type cd telegram-bot-capstone 
+### Followed by:
+> cd bin 
+
+> main.rb
+
+1. Download telegram on your system
+2. search for @morning_motivate_bot to chat with the bot.
+3. Enter /start and follow instructions
+
+# Enjoy Amazing benefits of the bot!
+
+__Here is an image to guide you how to use the bot:__
+
+![chatBot](/images/logo.png)
+
+
+
+# Installation
+Add following line to your Gemfile:
+
+> gem 'telegram-bot-ruby'
+
+And then execute:
+
+> $ bundle
+
+Or install it system-wide:
+
+> $ gem install telegram-bot-ruby
--- lib/bot.rb
+++ lib/bot.rb
@@ -1,6 +1,5 @@
 require 'telegram/bot'
 require_relative 'quotes.rb'
-require_relative '../bin/main.rb'
 
 class Bot
   def instructions
@@ -11,7 +10,7 @@ class Bot
   end
 
   def initialize
-    token = ''
+    token = '1484024670:AAGpphvzoWo4Ah6vtyjKnwNA7e8sgdXYT9E'
 
     Telegram::Bot::Client.run(token) do |bot|
       bot.listen do |message|
@@ -27,12 +26,30 @@ class Bot
           values = Quotes.new
           value = values.request_quote
           bot.api.send_message(chat_id: message.chat.id, text: value, date: message.date)
-        else bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name},'/n'
-          you need to use  /start,  /stop , /motivate or /joke")
+
+        # when '/question'
+        #   questions
+        else bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name},\n
+          you need to use  /start,  /stop , or /quotes")
         end
       end
     end
   end
-end
 
-Bot.new
+  # def questions
+  #   Telegram::Bot::Client.run(token) do |bot|
+  #     bot.listen do |message|
+  #       case message.text
+  #       when '/question'
+  #         question = 'London is a capital of which country?'
+  #         answers =
+  #           Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: `[%w(A B), %w(C D)]`, one_time_keyboard: true)
+  #         bot.api.send_message(chat_id: message.chat.id, text: question, reply_markup: answers)
+  #       when '/quit'
+  #         kb = Telegram::Bot::Types::ReplyKeyboardHide.new(hide_keyboard: true)
+  #         bot.api.send_message(chat_id: message.chat.id, text: 'Sorry to see you go :(', reply_markup: kb)
+  #       end
+  #     end
+  #   end
+  # end
+end
