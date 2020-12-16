57b2868127332051f29bcd928f7b782a45e25682
elisha2kyakpo1 <kyakopoe@gmail.com>
2020-12-16 17:02:06 +0200

    fixed typo errors in text

--- lib/bot.rb
+++ lib/bot.rb
@@ -7,7 +7,7 @@ class Bot
     "welcome to motivation chat bot created by Elisha Kyakopo,\n
     the chat bot is to keep you motivated and entertained.\n
     Use  /start to start the bot,  /stop to end the bot,\n
-    /quotes to get a diffrent motivational quotes"
+    /quotes to get a diffrent motivational quotes, or /facts to get data about something new"
   end
 
   def initialize
@@ -26,7 +26,7 @@ class Bot
         when '/facts'
           FactData.new.facts_data
         else bot.api.send_message(chat_id: message.chat.id, text: "Invalid entry, #{message.from.first_name},\n
-          you need to use  /start,  /stop , or /quotes")
+          you need to use  /start,  /stop ,/facts or /quotes")
         end
       end
     end
